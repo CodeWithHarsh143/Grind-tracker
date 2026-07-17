b3d8509bd7c6fad55784372b11f3949bb111f643
harsh <hk5324519@gmail.com/>
2026-07-17 21:25:10 +0530

    adding profile functionality

--- app/routes/profile.py
+++ app/routes/profile.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify, request
+from flask_login import current_user
+from ..model import User
 
 profile_bp = Blueprint("profile", __name__)
 
@@ -6,3 +8,17 @@ profile_bp = Blueprint("profile", __name__)
 @profile_bp.route("/profile", methods=["GET", "POST"])
 def profile():
     return render_template("profile.html")
+
+
+@profile_bp.route("/check-username")
+def checkUsername():
+    username = request.args.get("username", "").strip()
+    if username == current_user.username:
+        return jsonify({"available": True})
+    user = User.query.filter_by(username=username).first()
+
+    return jsonify(
+        {
+            "available": user is None  # return true if user is not avialable
+        }
+    )
